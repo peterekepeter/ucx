1020e22bb19d350b40f30f5dd1e0a47fb6260834
Eke Péter <peterekepeter@gmail.com>
2022-04-24 19:37:36 +0300

    SourceEditor implementation

--- src/lib/transpiler/SourceEditor.test.ts
+++ src/lib/transpiler/SourceEditor.test.ts
@@ -5,19 +5,37 @@ test("insert character", () =>{
     const editor = create("var x = 4;");
     editor.insert(0,4,'_');
     expect(editor.result).toBe("var _x = 4;");
-})
+});
 
 test("insert variable", () => {
-    const editor = create("var x = 9;")
+    const editor = create("var x = 9;");
     editor.insert(0,4,'i,');
-    expect(editor.result).toBe("var i,x = 9;")
-})
+    expect(editor.result).toBe("var i,x = 9;");
+});
 
 test('remove variable', () => {
-    const editor = create("var i,j,k,l;")
+    const editor = create("var i,j,k,l;");
     editor.remove(0,7,4);
-    expect(editor.result).toBe("var i,j;")
-})
+    expect(editor.result).toBe("var i,j;");
+});
+
+test('insert on second line', () => {
+    const editor = create("var i;\ni=0;");
+    editor.insert(1,0, '//');
+    expect(editor.result).toBe("var i;\n//i=0;");
+});
+
+test('maintains crlf', () => {
+    const editor = create("var i;\r\ni=0;");
+    expect(editor.result).toBe("var i;\r\ni=0;");
+});
+
+test('resolves order of edit' , () => {
+    const editor = create("var i,j;");
+    editor.replace(0,4,1,"index");
+    editor.replace(0,6,1,"count");
+    expect(editor.result).toBe("var index,count;");
+});
 
 function create(text: string) {
     return new SourceEditor(text);
--- src/lib/transpiler/SourceEditor.ts
+++ src/lib/transpiler/SourceEditor.ts
@@ -4,25 +4,47 @@
  * Applies edits to source
  */
 export class SourceEditor
-{
-    constructor(private _source: string){
+{   
+    sourceLines: string[];
+    edits: { position: number, length: number, text: string }[][];
+
+    constructor(source: string){
+        this.sourceLines = source.split('\n');
+        this.edits = this.sourceLines.map(line => []);
+    }
+
+    replace(line: number, position: number, replaceLength: number, withText: string){
+        this.edits[line].push({ position, length: replaceLength, text: withText });
     }
 
     insert(line: number, position: number, text: string)
     {
-        this._source = this._source.substring(0, position) + 
-            text + 
-            this._source.substring(position);
+        this.replace(line, position, 0, text);
     }
 
     remove(line: number, position: number, length: number)
     {
-        this._source = this._source.substring(0, position) + 
-            this._source.substring(position + length)
+        this.replace(line, position, length, '');
     }
 
     get result()
     {
-        return this._source;
+        const resultLines: string[] = [];
+        for (let i=0; i<this.sourceLines.length; i++){
+            let sourceLine = this.sourceLines[i];
+            const lineEdits = this.edits[i];
+            lineEdits.sort((a,b) => b.position - a.position);
+            for (const edit of lineEdits){
+                sourceLine = this.applyLineEdit(sourceLine, edit);
+            }
+            resultLines.push(sourceLine);
+        }
+        return resultLines.join('\n');
+    }
+
+    private applyLineEdit(input: string, edit: SourceEditor['edits'][0][0]){
+        return input.substring(0, edit.position) + 
+            edit.text + 
+            input.substring(edit.position + edit.length);
     }
 }
