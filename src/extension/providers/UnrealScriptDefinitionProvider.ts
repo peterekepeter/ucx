456f2a53bf258bf6371a84fc766ee02747b284e3
Eke Péter <peterekepeter@gmail.com>
2024-01-30 22:54:38 +0100

    Fix find classdef when typecasting to a class

--- src/extension/providers/UnrealScriptDefinitionProvider.ts
+++ src/extension/providers/UnrealScriptDefinitionProvider.ts
@@ -6,9 +6,7 @@ import { vscode } from '../vscode';
 export class UnrealScriptDefinitionProvider implements vscode.DefinitionProvider {
 
     provideDefinition(document: vscode.TextDocument, position: vscode.Position, ctoken: vscode.CancellationToken): vscode.ProviderResult<vscode.Definition | vscode.DefinitionLink[]> {
-        console.log('provideDefinition', { url: document.uri, position });
         return db.findDefinition(document.uri, position, ctoken).then((result: TokenInformation) => {
-            console.log('db.findDefinition result', result);
 
             let definition: vscode.DefinitionLink[] | null = null;
             if (result.found && result.token && result.uri) {
@@ -17,7 +15,7 @@ export class UnrealScriptDefinitionProvider implements vscode.DefinitionProvider
                     targetRange: rangeFromToken(result.token),
                 }];
             }
-            console.log({ output: definition });
+            
             return definition;
         });
     }
