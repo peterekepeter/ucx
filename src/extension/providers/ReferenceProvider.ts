d6bc7636a2cef41be533a4a67d1a9d42e4e2bfeb
Eke Péter <peterekepeter@gmail.com>
2024-10-20 13:38:00 +0200

    References provider use "includeDeclaration"

--- src/extension/providers/ReferenceProvider.ts
+++ src/extension/providers/ReferenceProvider.ts
@@ -5,11 +5,18 @@ import { vscode } from "../vscode";
 
 
 export class ReferenceProvider implements vscode.ReferenceProvider {
-
-    async provideReferences(document: vscode.TextDocument, position: vscode.Position, context: vscode.ReferenceContext, token: vscode.CancellationToken): Promise<vscode.Location[]> {
+    
+    async provideReferences(
+        document: vscode.TextDocument, position: vscode.Position, context: vscode.ReferenceContext, token: vscode.CancellationToken
+    ): Promise<vscode.Location[]> {
         db.updateDocumentAndGetAst(document, token);
         const output: vscode.Location[] = [];
-        const references = await db.findReferences(document, position, token);
+        let references = await db.findReferences(document, position, token);
+        
+        if (context.includeDeclaration === false) {
+            // assuming first reference is always the delcaration
+            references = references.slice(1);
+        }
         
         if (token.isCancellationRequested) {
             return output;
